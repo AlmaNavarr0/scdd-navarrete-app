712a3d7b5ccf3c6daaf189dfd58462a0a2aeea37
DianaBelenNavarro <93990168+DianaBelenNavarro@users.noreply.github.com>
2022-08-06 19:22:36 -0500

    06/08/ 19:22 hrs

--- index.js
+++ index.js
@@ -43,7 +43,5 @@ app.use('/', trabajador);
 app.use('/', flotilla); 
 app.use('/', permiso);
 
-app.listen(process.env.PORT || 8080, function() {
-console.log('Servidor escuchando en el puerto 8080');
-
-});
+const port = process.env.POST || 3000;
+app.listen(port, ()=> console.log(`Puerto escuchando ${port}`));
